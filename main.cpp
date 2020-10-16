55d72a1a38ac6333a78063fc3d06aa17185eb190
Vitor de Castro Carvalho <oi.vitor@gmail.com>
2020-10-16 10:59:38 +0200

    Clean main code

--- main.cpp
+++ main.cpp
@@ -1,9 +1,4 @@
-
-#include "opencv2/core/core.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc.hpp"
 #include "iostream"
-//#include "functional"
 
 #include "ProcessingManager.h"
 
@@ -27,37 +22,6 @@ int main()
 	{
 		processingManagerPtr->updateApplication();
 	}
-	
 
-	/*
-	auto imageManagerPtr = std::make_unique<ImageManager>();
-
-	
-
-	// Reading an image
-	std::string image_path = cv::samples::findFile("data/game/niveau1.png"); // TODO : Check before if path exists
-
-	if (!imageManagerPtr)
-	{
-		std::cout << "imageManagerPtr is null !! " << std::endl;
-		return 1;
-	}
-
-	cv::Mat field = imageManagerPtr->getLastImage();
-
-	if (field.empty())
-	{
-		std::cout << "Could not read the image: " << image_path << std::endl;
-		return 1;
-	}
-
-	cv::imshow("Display window", field);
-	int k = cv::waitKey(0); // Wait for a keystroke in the window
-
-	if (k == 's')
-	{
-		cv::imwrite("segmented_field.png", field);
-	}
-	*/
 	return 0;
 }
